d680a5a91f5e13637801c782406a0c2ce535b4f4
Tanmai7 <tanmaisodisetti@gmail.com>
2026-02-05 23:09:11 +0530

    Initialized Streamlit dashboard framework (Step 8.1)

---
+++ dashboard/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI-Based RFI Detection", layout="wide")
+
+st.title("AI-Based Radio Frequency Interference Detection & Classification")
+
+st.write("Dashboard initialization successful.")
